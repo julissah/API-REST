19addc9a338ce8d25dbe80cbd94cd953c54124a7
Julissa Huaman Hilari <julissa.2606@gmail.com>
2021-10-19 23:53:38 -0500

    feat: add cors

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes/index');
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+
 
 const app = express();
 const port = 3002;
 // Permite recibir informacion de tipo JSON que nos envian por POST
 app.use(express.json());
+// Si nuestra API no es publica y solo queremos que solo algunos dominios puedan acceder
+// podemos crear un array con los dominios que tendran acceso
+const whiteList = [
+  'http://localhost:8080',
+  'https: //myapp.com'
+];
+const options = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+
+// para utlizar cors, se utiliza como un middleware
+// ahora nuestra API aceptara cualquier origen  de cualquier dominio
+app.use(cors(options));
 
 app.get('/', (req,res) => {
   res.send('Hola mi server en express');
--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -1,9 +1,11 @@
-const { boom } = require("@hapi/boom");
+const boom = require('@hapi/boom');
 
+// Retorna una funcion
 function validatorHandler (schema, property) {
+  // Estamos creando un middleware dinamico
   return(req, res, next) => {
     const data = req[property];
-    const { error } = schema.validate(data);
+    const { error } = schema.validate(data, { abortEarly: false });
     if (error) {
       // badRequest envia un error de tipo 400
       next(boom.badRequest(error));
